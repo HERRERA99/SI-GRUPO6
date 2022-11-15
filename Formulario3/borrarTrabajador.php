aa2cb49f29fe812d94461cf500585512fe999fd0
HERRERA99 <aitor001970@gmail.com>
2022-11-15 20:19:26 +0100

    Updated

--- Formulario3/borrarTrabajador.php
+++ Formulario3/borrarTrabajador.php
@@ -1,59 +1,62 @@
-<html>
-	<head>
-		<title>Borrar</title>
-	</head>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>BORRAR TRABAJADOR</title>
+</head>
+<header>
+    <div class="container">
+        <img src="logo.png" class="logo">
+        <h1 class="logo">
+            LA SUPERMODA
+        </h1>
+    </div>
+</header>
+		
 	<body>
-	<h1>Datos borrados</h1>
-		
-		
-		<?PHP
-			
-			$dni=$_POST["dni"];
-			
-			
-			
-			if(file_exists("trabajadores.xml")){
-				$encontrado=-1;
-				$trabajadores=simplexml_load_file("trabajadores.xml");
-				$contador = -1; 
-				foreach($trabajadores->nuevoTrabajador as $trabajador){
-                    $contador++;;
-					if ($trabajador["dni"]==$dni){
-						$encontrado=$contador;
-                        echo"<p> Se encontró un trabajador con estos datos:</p>";
-						echo "<ul>";
-							echo "<li>Nombre: ".$trabajador->nombre."</li>";
-							echo "<li>Apellidos: ".$trabajador->apellidos."</li>";
-							echo "<li>DNI: ".$trabajador["dni"]."</li>";
-						echo "</ul>";
-						
-                       
-						
-						
+		<div class="datosBorrado" style="margin:50px">
+			<?PHP
+				
+				$dni=$_POST["dni"];
+				
+				
+				
+				if(file_exists("trabajadores.xml")){
+					$encontrado=-1;
+					$trabajadores=simplexml_load_file("trabajadores.xml");
+					$contador = -1; 
+					foreach($trabajadores->nuevoTrabajador as $trabajador){
+						$contador++;;
+						if ($trabajador["dni"]==$dni){
+							$encontrado=$contador;
+							echo"<p> Se encontró un trabajador con estos datos:</p>";
+							echo "<ul>";
+								echo "<li>Nombre y Apellidos: ".$trabajador->nombreApellidos."</li>";
+								echo "<li>DNI: ".$trabajador["dni"]."</li>";
+							echo "</ul>";
+						}
 					}
-				}
 
-				if($encontrado>=0){
-                        unset($trabajadores->nuevoTrabajador[$encontrado]);                        
-                        if($trabajadores->asXML("trabajadores.xml")){
-                            echo"<p>Se ha dado de baja al trabajador</p>";
-                        
-                        
-                        }else{
-                            
-							echo"<p>No se pudo actualizar el archivo</p>";
-                        }
+					if($encontrado>=0){
+							unset($trabajadores->nuevoTrabajador[$encontrado]);                        
+							if($trabajadores->asXML("trabajadores.xml")){
+								echo"<p>Se ha dado de baja al trabajador</p>";
+							}else{
+								echo"<p>No se pudo actualizar el archivo</p>";
+							}
+						
+					}else{
+						echo"<p>No se ha encontrado ningún usuario con este DNI: $dni</p>";
+					}
+									
 					
 				}else{
-					echo"<p>No se ha encontrado ningún usuario con ese DNI</p>";
+					exit("No se pudo abrir el archivo trabajadores.xml");
 				}
-								
-				
-			}else{
-				exit("No se pudo abrir el archivo trabajadores.xml");
-			}
-	 	?>
-		
-		
+			?>
+		</div>
 	</body>
 </html>
--- Formulario3/indexBorrarTrabajador.php
+++ Formulario3/indexBorrarTrabajador.php
@@ -1,17 +1,20 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="index.css">
-		<title>Formulario</title>
-	</head>
-
-    <header>
-        <div class="container">
-            <img src="logo.png" class="logo">
-            <h1 class="logo">
-                LA SUPERMODA
-            </h1>
-        </div>
-    </header>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>BORRAR TRABAJADOR</title>
+</head>
+<header>
+    <div class="container">
+        <img src="logo.png" class="logo">
+        <h1 class="logo">
+            LA SUPERMODA
+        </h1>
+    </div>
+</header>
 
 
 	<body>
